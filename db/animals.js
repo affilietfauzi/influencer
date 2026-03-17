f8d5a346095902045bcc4a552c23f693045ff75b
affilietfauzi <affilietfauzi@gmail.com>
2026-03-17 20:13:28 +0700

    Update animals.js

--- db/animals.js
+++ db/animals.js
@@ -1,10 +1,8 @@
 export const animals = [
-"a tiger",
-"a leopard",
-"a white tiger",
-"a lion",
-"a black panther",
-"a wolf",
-"a fox",
-"none"
+{ name:"None", desc:"no animal" },
+{ name:"Leo", desc:"a majestic lion calmly sitting behind her" },
+{ name:"Tiger", desc:"a calm tiger resting behind her" },
+{ name:"Panther", desc:"a sleek black panther relaxing nearby" },
+{ name:"Wolf", desc:"a calm grey wolf lying peacefully" },
+{ name:"Fox", desc:"a curious red fox sitting near her feet" }
 ]
