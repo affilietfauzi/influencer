ca5a2559c8c8858e2f7ba6cc0b92b37b4bd32c7e
affilietfauzi <affilietfauzi@gmail.com>
2026-03-17 20:04:04 +0700

    Create influencers.js

---
+++ db/influencers.js
@@ -0,0 +1,11 @@
+export const influencers = [
+{ name:"Default Korean Influencer", desc:"young korean female influencer" },
+
+{ name:"Emma (USA)", desc:"american influencer blonde hair" },
+{ name:"Sofia (Spain)", desc:"spanish influencer tan skin" },
+{ name:"Yuki (Japan)", desc:"japanese influencer cute style" },
+{ name:"Ananya (India)", desc:"indian influencer elegant style" },
+{ name:"Lina (UAE)", desc:"middle eastern luxury influencer" },
+{ name:"Camila (Brazil)", desc:"brazilian influencer beach vibe" },
+{ name:"Alya (Indonesia)", desc:"indonesian hijab influencer" }
+]
